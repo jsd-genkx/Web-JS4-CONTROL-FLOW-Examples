0b719201e2a8a12df45a1a227a7cfccd258a3caf
Khem <tempkhem@gmail.com>
2024-10-09 09:24:53 +0700

    feat(goToSchool.js)

---
+++ goToSchool.js
@@ -0,0 +1,22 @@
+  // const normalPerson = {
+  //   name: "A",
+  //   isStudent: true
+  // }
+
+  // if (normalPerson.isStudent) {
+  //   console.log("GO TO SCHOOL");
+  // } else {
+  //   console.log("GO SOMEWHERE ELSE");
+  // }
+
+
+  const mrNew = {
+    name: "New",
+    isStudent: false
+  }
+
+  if (mrNew.isStudent) {
+    console.log("GO TO SCHOOL (FROM BACKDOOR)")
+  } else {
+    console.log("GO TO WORK");
+  }
